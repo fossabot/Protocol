57a24a91025b3b7b4eff1e6ed9b3a5df9d97de00
Harry Fox <harry.fox.ic@icloud.com>
2017-12-24 16:27:47 +0000

    Test git hooks

---
+++ src/test/java/org/laxio/piston/protocol/v001/packet/handshake/server/HandshakePacketTest.java
@@ -0,0 +1,15 @@
+package org.laxio.piston.protocol.v001.packet.handshake.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandshakePacketTest {
+
+    @Test
+    void read() {
+        // TODO: create test
+        // test
+    }
+
+}
