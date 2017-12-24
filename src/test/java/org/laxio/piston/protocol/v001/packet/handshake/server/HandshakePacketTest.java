8143afc8d8b3b9a963576136088e38c987880274
Harry Fox <harry.fox.ic@icloud.com>
2017-12-24 15:12:59 +0000

    Add test for HandshakePacket

---
+++ src/test/java/org/laxio/piston/protocol/v001/packet/handshake/server/HandshakePacketTest.java
@@ -0,0 +1,14 @@
+package org.laxio.piston.protocol.v001.packet.handshake.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandshakePacketTest {
+
+    @Test
+    void read() {
+        // TODO: create test
+    }
+
+}
