c8ba02b3c88374f5f6943524715d8afcc8645f6b
Harry Fox <harry.fox.ic@icloud.com>
2017-12-27 10:08:06 +0000

    Add EntityType
    Signed-off-by: Harry Fox <harry.fox.ic@icloud.com>

--- src/main/java/org/laxio/piston/protocol/v340/packet/play/client/SpawnObjectPacket.java
+++ src/main/java/org/laxio/piston/protocol/v340/packet/play/client/SpawnObjectPacket.java
@@ -1,6 +1,7 @@
 package org.laxio.piston.protocol.v340.packet.play.client;
 
 import org.laxio.piston.piston.entity.Entity;
+import org.laxio.piston.piston.entity.type.ObjectEntity;
 import org.laxio.piston.piston.protocol.stream.PistonOutput;
 import org.laxio.piston.protocol.v340.packet.ProtocolPacket;
 
@@ -8,13 +9,13 @@ import java.io.IOException;
 
 public class SpawnObjectPacket extends ProtocolPacket {
 
-    private Entity entity;
+    private ObjectEntity entity;
 
     public SpawnObjectPacket() {
         // required empty packet
     }
 
-    public SpawnObjectPacket(Entity entity) {
+    public SpawnObjectPacket(ObjectEntity entity) {
         this.entity = entity;
     }
 
@@ -26,7 +27,7 @@ public class SpawnObjectPacket extends ProtocolPacket {
     public void onWrite(PistonOutput output) throws IOException {
         output.writeVarInt(entity.getEntityId());
         output.writeUUID(entity.getUUID());
-        output.writeByte(entity.getType().getId());
+        output.writeByte(entity.getObjectType().getId());
         output.writeLocation(entity.getLocation(), true);
         output.writeInt(entity.getData());
         output.writeVelocity(entity.getVelocity());
